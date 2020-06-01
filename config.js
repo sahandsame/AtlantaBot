6152902f8a42bdb1c341a10bc21c933ff2d9c5b9
sahandsame <58979786+sahandsame@users.noreply.github.com>
2020-06-01 17:42:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "NzE2OTY3NjU5NDM0MTQ3OTMw.XtUSBg.CROvf6UjDEM9BR9qZcjp09R2lhU",
+	token: "NzE2OTY3NjU5NDM0MTQ3OTMw.XtUTvw.2lDZ2UmEbGTCMNwjPlYlJNPutgY",
 	/* For the support server */
 	support: {
 		id: "XXXXXXXXXXX", // The ID of the support server
